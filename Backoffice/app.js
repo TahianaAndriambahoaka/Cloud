f424b2f4bc306821d43a7800727cef494377ad49
Tsantaniaina <tsantaniainarakotonjanahary@gmail.com>
2022-01-25 01:01:13 +0300

    dfdgdg

--- Backoffice/app.js
+++ Backoffice/app.js
@@ -6,7 +6,6 @@ var app = angular.module('myApp', []);
 
   app.controller('myCtrl', function($window,$scope,$http) 
   {
-     /*console.log("a");
 
     $scope.versAffectation = function(idSignalement)
     {
@@ -50,17 +49,23 @@ var app = angular.module('myApp', []);
       
     }
 
-    //Liste signalement non affecté
+    /*//Liste signalement non affecté
     $http.get("http://localhost:8072/listNewSignalement").then(function(response) {
         console.log(response.data);
         $scope.listNewSignalement = response.data;
         //console.log(response.data[0]);
+    });*/
+
+    
+    $http({ method: 'GET',url: 'http://localhost:8072/listNewSignalement',headers: { 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZHZ3ZGZkZGJAZG1pa2puY2RrbsOpY2QiLCJpYXQiOjE2NDMwNTk4MjQsImV4cCI6MTY0MzE0NjIyNH0.mit-MuuFrQo1mh3CSbpro8KyR6zWyG8LeD9ldADsD6RRbc9JHl2MlGtW10vSMdeoFVxw9wdKnVGVPn42NKym7g' ,'Content-Type': 'application/json','Accept':'application/json'}}).then(function successCallback(response) 
+    {
+          console.log(response.data);
+          $scope.listNewSignalement = response.data;
+    }, function errorCallback(response) {
+      
+          console.log(response.status);
     });
 
-    //Liste signalement affecté
-    $http.get("http://localhost:8072/listAffectedSignalement").then(function(response) {      
-        $scope.listAffectedSignalement = response.data;
-    });*/
 
     $http({ method: 'GET',url: 'http://localhost:8072/listAffectedSignalement',headers: { 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZHZ3ZGZkZGJAZG1pa2puY2RrbsOpY2QiLCJpYXQiOjE2NDMwNTk4MjQsImV4cCI6MTY0MzE0NjIyNH0.mit-MuuFrQo1mh3CSbpro8KyR6zWyG8LeD9ldADsD6RRbc9JHl2MlGtW10vSMdeoFVxw9wdKnVGVPn42NKym7g' ,'Content-Type': 'application/json','Accept':'application/json'}}).then(function successCallback(response) 
     {
@@ -71,6 +76,14 @@ var app = angular.module('myApp', []);
     });
 
     $http({ method: 'GET',url: 'http://localhost:8072/regions',headers: { 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZHZ3ZGZkZGJAZG1pa2puY2RrbsOpY2QiLCJpYXQiOjE2NDMwNTk4MjQsImV4cCI6MTY0MzE0NjIyNH0.mit-MuuFrQo1mh3CSbpro8KyR6zWyG8LeD9ldADsD6RRbc9JHl2MlGtW10vSMdeoFVxw9wdKnVGVPn42NKym7g' ,'Content-Type': 'application/json','Accept':'application/json'}}).then(function successCallback(response) 
+    {
+          console.log(response.data)
+    }, function errorCallback(response) {
+      
+          console.log(response.status);
+    });
+
+    $http({ method: 'POST',url: 'http://localhost:8072/region',headers: { 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZHZ3ZGZkZGJAZG1pa2puY2RrbsOpY2QiLCJpYXQiOjE2NDMwNTk4MjQsImV4cCI6MTY0MzE0NjIyNH0.mit-MuuFrQo1mh3CSbpro8KyR6zWyG8LeD9ldADsD6RRbc9JHl2MlGtW10vSMdeoFVxw9wdKnVGVPn42NKym7g' ,'Content-Type': 'application/json','Accept':'application/json'},data : {nom:'Analamanga'}}).then(function successCallback(response) 
     {
           console.log(response.data)
     }, function errorCallback(response) {
