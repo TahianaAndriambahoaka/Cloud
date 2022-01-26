b2145620c10fa1171c7eb177f8994fc921675528
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2022-01-26 15:38:10 +0300

    push

--- Backoffice/js/ficheApp.js
+++ Backoffice/js/ficheApp.js
@@ -16,6 +16,7 @@
     function myCtrl($window, $scope, $http) {
 
         $scope.getOneSignalement = function(idSignalement) {
+            console.log("jfsdjkfkasjdfka");
             $http.get('http://localhost:8072/signalement/' + idSignalement).then(function successCallback(response) {
                 var photo = response.data[0][9];
                 // $scope.photo = "storage/" + response.data;
@@ -50,7 +51,31 @@
         $scope.getOneSignalement(sessionStorage.getItem("idSignalement"));
         $scope.getListRegion();
 
+
+
+        // $scope.showDetails = function(id) {
+        //     alert('lat: ' + id.lat + ', ' + 'lon: ' + id.lon);
+        // };
+        // angular.extend($scope, {
+        //     center: {
+        //         lat: -18.933333,
+        //         lon: 47.516667,
+        //         zoom: 9
+        //     },
+        //     finisterre: {
+        //         lat: -18.933333,
+        //         lon: 47.516667,
+        //         label: {
+        //             show: false
+        //         },
+        //         onClick: function(event, properties) {
+        //             alert('lat: ' + properties.lat + ', ' + 'lon: ' + properties.lon);
+        //         }
+        //     }
+        // });
+
+
     }
 
-    angular.module('myApp', []).factory('authInterceptor', authInterceptor).config(function($httpProvider) { $httpProvider.interceptors.push('authInterceptor'); }).controller('main', myCtrl)
+    angular.module('myApp', ["openlayers-directive"]).factory('authInterceptor', authInterceptor).config(function($httpProvider) { $httpProvider.interceptors.push('authInterceptor'); }).controller('main', myCtrl)
 })();
--- Backoffice/map/script.js
+++ Backoffice/map/script.js
@@ -1,10 +1,10 @@
 var app = angular.module("demoapp", ["openlayers-directive"]);
 
 app.controller("DemoController", ['$scope', function($scope) {
-  $scope.showDetails = function(id) { alert('lat: '+ id.lat+', '+'lon: '+id.lon); };
-  angular.extend($scope, {
-    center: { lat: 42.9515,lon: -8.6619, zoom: 9 },
-    finisterre: { lat: 30.907800500000000000,lon: 30.265031499999964000,label: { show: false },onClick: function (event, properties) { alert('lat: '+ properties.lat+', '+'lon: '+properties.lon); } }
-  });
+    $scope.showDetails = function(id) { alert('lat: ' + id.lat + ', ' + 'lon: ' + id.lon); };
+    angular.extend($scope, {
+        center: { lat: -18.933333, lon: 47.516667, zoom: 9 },
+        finisterre: { lat: -18.933333, lon: 47.516667, label: { show: false }, onClick: function(event, properties) { alert('lat: ' + properties.lat + ', ' + 'lon: ' + properties.lon); } }
+    });
 
 }]);
