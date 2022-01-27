9b0f1e41ae419c30dae7fd2150176b65eea1fbfe
Tsantaniaina <tsantaniainarakotonjanahary@gmail.com>
2022-01-28 02:17:19 +0300

    jvjvjbjb
    kbi

--- Backoffice/js/ficheApp.js
+++ Backoffice/js/ficheApp.js
@@ -1,6 +1,5 @@
-;
-(function() {
-
+;(function() 
+{
     function authInterceptor($window) 
     {
         return {
@@ -18,44 +17,30 @@
 
         $scope.getOneSignalement = function(idSignalement) 
         {
-            console.log("jfsdjkfkasjdfka");
-            $http.get('http://localhost:8072/signalement/' + idSignalement).then(function successCallback(response) 
-            {
-                var photo = response.data[0][9];
-                document.getElementById("pic-container").innerHTML = "<img src = 'storage/" + photo + "' alt = 'Une erreur s est produite lors du chargement de l image'>";
-            },
-            function errorCallback(response) {
-                if (response.status == 401) { $window.location.href = 'login.html' }
-            });
-
             $http.get('http://localhost:8072/signalement/' + idSignalement).then(function successCallback(response) 
             {
                 $scope.oneSignalement = response.data
-                window.localStorage['latitude'] = $scope.oneSignalement[0][7];
-                window.localStorage['longitude'] = $scope.oneSignalement[0][8];
-            },
-            function errorCallback(response) {
-                if (response.status == 401) { $window.location.href = 'login.html' }
-            });
-        }
-        $scope.affecterSignalement = function(idSignalement) {
-            // console.log("idS: "+idSignalement);
-            // console.log("idReg: "+$scope.idRegion);
-            $http.put('http://localhost:8072/affecterSignalement/' + idSignalement + '/' + $scope.idRegion).then(function successCallback(response) {
-                    $window.location.reload()
-                },
-                function errorCallback(response) {
-                    if (response.status == 401) { $window.location.href = 'login.html' }
-                });
+                $scope.position.lat = $scope.oneSignalement[0][7];
+                $scope.position.lon = $scope.oneSignalement[0][8];
+                document.getElementById("pic-container").innerHTML = "<img src = 'storage/" + $scope.oneSignalement[0][9] + "' alt = 'Une erreur s est produite lors du chargement de l image'>";
+            },function errorCallback(response) { if (response.status == 401) { $window.location.href = 'login.html' } });
         }
 
-        $scope.getListRegion = function(id) {
-            $http.get('http://localhost:8072/regions').then(function successCallback(response) {
-                    $scope.listeRegion = response.data
-                },
-                function errorCallback(response) {
-                    if (response.status == 401) { $window.location.href = 'login.html' }
-                });
+
+        $scope.affecterSignalement = function(idSignalement) 
+        {
+            $http.put('http://localhost:8072/affecterSignalement/' + idSignalement + '/' + $scope.idRegion).then(function successCallback(response) 
+            {
+                    $window.location.reload()
+            },function errorCallback(response) { if (response.status == 401) { $window.location.href = 'login.html' } });
+        }
+
+        $scope.getListRegion = function(id) 
+        {
+            $http.get('http://localhost:8072/regions').then(function successCallback(response) 
+            {
+                $scope.listeRegion = response.data
+            },function errorCallback(response) { if (response.status == 401) { $window.location.href = 'login.html' } });
         }
 
         //ON LOAD  
@@ -69,9 +54,9 @@
                  lon: 47.516667,
                  zoom: 5
              },
-             finisterre: {
-                 lat:  window.localStorage['latitude'],
-                 lon:  window.localStorage['longitude'],
+             position: {
+                 lat:  -18.933333,
+                 lon:  47.516667,
                  label: { show: false},
                  onClick: function(event, properties) {
                      alert('lat: ' + properties.lat + ', ' + 'lon: ' + properties.lon);
@@ -79,7 +64,7 @@
              }
          });
 
-
+         
     }
 
     angular.module('myApp', ["openlayers-directive"]).factory('authInterceptor', authInterceptor).config(function($httpProvider) { $httpProvider.interceptors.push('authInterceptor'); }).controller('main', myCtrl)
